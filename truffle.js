87afcd679ce11d8df3eb74eebc5202b40f6795a0
Dev Bharel <dev@spacemanholdings.com>
2018-08-21 15:23:33 -0500

    added truffle deploy and basic testing skeleton

--- truffle.js
+++ truffle.js
@@ -15,4 +15,9 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  develop: {
+    host: "127.0.0.1",
+    port: 9545,
+    network_id: "*"
+  }
 };
