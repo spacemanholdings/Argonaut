cf5b49966b189aebf70fcc21e5f3fdf8467c4b63
Dev Bharel <dev@spacemanholdings.com>
2018-09-16 04:03:40 -0500

    setting up front end

--- AG_Holacracy/Token/interfaces/IERC677Reciever.sol
+++
@@ -1,6 +0,0 @@
-/* This interface is from https://github.com/poanetwork/poa-bridge-contracts*/
-pragma solidity ^0.4.23;
-
-interface ERC677Receiver {
-  function onTokenTransfer(address _from, uint _value, bytes _data) external returns(bool);
-}
