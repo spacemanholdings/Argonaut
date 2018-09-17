f692a885c39aaf158d15000655c16f72e1e42123
Dev Bharel <dev@spacemanholdings.com>
2018-09-17 00:13:43 -0500

    circle role and domain works now

--- nh.js
+++ nh.js
@@ -1,8 +1,8 @@
 let namehash = require('eth-ens-namehash');
 let k3 = require('js-sha3').keccak_256;
 
-let label = "kelvin"
-let str = label + ".arg"
+let label = "members"
+let str = label + ".b1.arg"
 let labelhash = k3(label);
 let nameHash = namehash.hash(str);
 console.log(str, "0x"+labelhash, nameHash);
