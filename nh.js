bd07d8ec306870450705e08ff2ae0d5cf6e61801
Dev Bharel <dev@spacemanholdings.com>
2018-09-17 00:43:47 -0500

    added transfer functionality to transfer token

--- nh.js
+++ nh.js
@@ -1,7 +1,7 @@
 let namehash = require('eth-ens-namehash');
 let k3 = require('js-sha3').keccak_256;
 
-let label = "members"
+let label = "leadlink"
 let str = label + ".b1.arg"
 let labelhash = k3(label);
 let nameHash = namehash.hash(str);
